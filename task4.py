d1e75e965ecd3f759394501850c8cb903a577089
Julia Zahorovska <ju.zagorovsky@gmail.com>
2025-12-31 09:55:57 +0100

    add readme description and initial code for task 4, 6

---
+++ task4.py
@@ -0,0 +1,66 @@
+# Наступний код виконує побудову бінарних дерев. Виконайте аналіз коду, щоб зрозуміти, як він працює.
+
+import uuid
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(
+            node.id, color=node.color, label=node.val
+        )  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2**layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2**layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]["color"] for node in tree.nodes(data=True)]
+    labels = {
+        node[0]: node[1]["label"] for node in tree.nodes(data=True)
+    }  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(
+        tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors
+    )
+    plt.show()
+
+
+# Створення дерева
+root = Node(0)
+root.left = Node(4)
+root.left.left = Node(5)
+root.left.right = Node(10)
+root.right = Node(1)
+root.right.left = Node(3)
+
+# Відображення дерева
+draw_tree(root)
+
+
+# Використовуючи як базу цей код, побудуйте функцію, що буде візуалізувати бінарну купу.
+#  👉🏻 Примітка. Суть завдання полягає у створенні дерева із купи.
---
+++ task6.py
@@ -0,0 +1,8 @@
+items = {
+    "pizza": {"cost": 50, "calories": 300},
+    "hamburger": {"cost": 40, "calories": 250},
+    "hot-dog": {"cost": 30, "calories": 200},
+    "pepsi": {"cost": 10, "calories": 100},
+    "cola": {"cost": 15, "calories": 220},
+    "potato": {"cost": 25, "calories": 350},
+}
